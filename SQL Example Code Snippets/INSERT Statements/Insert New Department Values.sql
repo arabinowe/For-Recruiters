9d559e3c8f001125bab28fe928f3023d516f23cd
arabinoweRG <120216334+arabinoweRG@users.noreply.github.com>
2023-01-06 13:22:22 -0600

    Added INSERT Snippets

---
+++ SQL Example Code Snippets/INSERT Statements/Insert New Department Values.sql
@@ -0,0 +1,15 @@
+SELECT *
+FROM departments;
+
+INSERT INTO departments
+(
+dept_no,
+dept_name
+)
+VALUES
+(
+'d010',
+'Business Analysis'
+);
+
+SELECT * FROM departments;
---
+++ SQL Example Code Snippets/INSERT Statements/Table Duplication - all values copy insertion.sql
@@ -0,0 +1,19 @@
+SELECT *
+FROM departments;
+
+CREATE TABLE departments_dup (
+    dept_no CHAR(4) NOT NULL,
+    dept_name VARCHAR(40) NOT NULL
+);
+
+INSERT INTO departments_dup(
+dept_no,
+dept_name
+)
+SELECT *
+FROM departments;
+
+SELECT 
+    *
+FROM
+    departments_dup;
