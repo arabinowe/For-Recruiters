cf74dd9da99f972ca5467ef2e46a5d7221dd6664
arabinoweRG <120216334+arabinoweRG@users.noreply.github.com>
2023-01-06 13:30:21 -0600

    Create UPDATE SET existing record.sql

---
+++ SQL Example Code Snippets/Transaction Control Statements/UPDATE/UPDATE SET existing record.sql
@@ -0,0 +1,14 @@
+USE employees;
+
+UPDATE employees
+SET
+	first_name='Stella',
+    last_name='Parkinson',
+    birth_date='1990-12-31',
+    gender='F'
+WHERE
+	emp_no=999901;
+
+SELECT *
+FROM employees
+WHERE emp_no=999901;
