fb63ab5e3498a50c6459186bac3e2233251c3177
arabinoweRG <120216334+arabinoweRG@users.noreply.github.com>
2023-01-08 17:17:05 -0600

    Create LEFT JOIN Multiple Order By and WHERE clause.sql

---
+++ SQL Example Code Snippets/JOINs/LEFT JOIN Multiple Order By and WHERE clause.sql
@@ -0,0 +1,9 @@
+SELECT 
+    e.emp_no, e.first_name, e.last_name, m.dept_no, m.from_date
+FROM
+    employees e
+        LEFT JOIN
+   dept_manager m  ON e.emp_no = m.emp_no
+WHERE e.last_name='Markovitch'
+ORDER BY m.dept_no DESC, e.emp_no;
+
