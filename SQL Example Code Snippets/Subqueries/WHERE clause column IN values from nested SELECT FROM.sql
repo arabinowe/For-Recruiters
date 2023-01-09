6129dfeded7950e214e65d8aa05fed805a80a313
arabinoweRG <120216334+arabinoweRG@users.noreply.github.com>
2023-01-08 19:49:07 -0600

    Update WHERE clause column IN values from nested SELECT FROM.sql

--- SQL Example Code Snippets/Subqueries/WHERE clause column IN values from nested SELECT FROM.sql
+++ SQL Example Code Snippets/Subqueries/WHERE clause column IN values from nested SELECT FROM.sql
@@ -1,12 +1,9 @@
-SELECT first_name, last_name
-
+SELECT 
+    first_name, last_name
 FROM
-employees e
-
+    employees e
 WHERE
-e.emp_no IN (
-SELECT
-dm.emp_no
-FROM
-dept_manager dm
-);
+    e.emp_no IN (SELECT 
+            dm.emp_no
+        FROM
+            dept_manager dm);
