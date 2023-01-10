51840dd15ebaeff81b394a5c699f2463d0e4dc8a
arabinoweRG <120216334+arabinoweRG@users.noreply.github.com>
2023-01-10 01:33:59 -0600

    Update Double nested subquery with JOIN and EXISTS.sql

--- SQL Example Code Snippets/Subqueries/Double nested subquery with JOIN and EXISTS.sql
+++ SQL Example Code Snippets/Subqueries/Double nested subquery with JOIN and EXISTS.sql
@@ -1,4 +1,3 @@
-### I'll be honest and say I'm not sure if this is a good query, but it's a functional second order subquery with a join
 ### second subquery requires an alias to be joined, it won't just grab it without giving it an assignment
 
 SELECT * FROM (SELECT * FROM employees e
